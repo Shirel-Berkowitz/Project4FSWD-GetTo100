71756456aae360e05e787e90436c268d3768756c
Shirel <shkadosh@g.jct.ac.il>
2023-04-20 11:41:31 +0300

    add player

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,30 @@ class App extends Component {
       steps: 0,
       isDisabledBtn: false,
       players: [
-        { name: "Shirel", value: 0 },
-        { name: "Ori", value: 0 },
+        { name: "Shirel", score: 0 },
+        { name: "Ori", score: 0 },
       ],
       currentPlayerIndex: 0,
       gameOver: false,
     };
     this.handlePlayerAction = this.handlePlayerAction.bind(this);
   }
+
+  //add a new player to the array of players
+  addNewPlayer(name) {
+    const newPlayer = {
+      name: name,
+      score: 0,
+    };
+    console.log(`Adding player ${this.state.username}`);
+    this.setState((prevState) => ({
+      players: [...prevState.players, newPlayer],
+    }));
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ username: event.target.value });
+  };
 
   handlePlayerAction = (value) => {
     this.setState((prevState) => {
@@ -40,12 +56,6 @@ class App extends Component {
       };
     });
   };
-
-  // addGamerButton() {
-  //   this.setState((username, score = 0) => ({
-  //     //gamers: this.state.gamers.push()
-  //   }));
-  // }
 
   //???
   // addGamer() {
@@ -76,38 +86,51 @@ class App extends Component {
   render() {
     const { isDisabledBtn } = this.state;
     return (
-      <div className="App">
+      <div>
         <h1>Get to 100!</h1>
-        <h2>User: {this.state.username}</h2>
-        <h4>{this.state.isDisabledBtn == true ? "Enabled!" : "Disabled"}</h4>
-        <h2>{this.state.count}</h2>
-        <h3>steps: {this.state.steps}</h3>
-        <h4>{this.state.count == 100 ? "You won!" : ""}</h4>
-        <div className="actionBtns">
-          <button
-            onClick={() => this.handlePlayerAction("+1")}
-            disabled={isDisabledBtn}
-          >
-            +1
+        <div className="Sign-up">
+          <h2>Hello!</h2>
+          <h2>Please enter your name:</h2>
+          <input type="text" onChange={this.handleInputChange}></input>
+          <br></br>
+          <button onClick={() => this.addNewPlayer(this.state.username)}>
+            Add another player
           </button>
-          <button
-            onClick={() => this.handlePlayerAction("-1")}
-            disabled={isDisabledBtn}
-          >
-            -1
-          </button>
-          <button
-            onClick={() => this.handlePlayerAction("×2")}
-            disabled={isDisabledBtn}
-          >
-            ×2
-          </button>
-          <button
-            onClick={() => this.handlePlayerAction("÷2")}
-            disabled={isDisabledBtn}
-          >
-            ÷2
-          </button>
+          <button>Done, let's play!</button>
+        </div>
+
+        <div className="App" style={{ visibility: "collapse" }}>
+          <h2>User: {this.state.username}</h2>
+          <h4>{this.state.isDisabledBtn == true ? "Enabled!" : "Disabled"}</h4>
+          <h2>{this.state.count}</h2>
+          <h3>steps: {this.state.steps}</h3>
+          <h4>{this.state.count == 100 ? "You won!" : ""}</h4>
+          <div className="actionBtns">
+            <button
+              onClick={() => this.handlePlayerAction("+1")}
+              disabled={isDisabledBtn}
+            >
+              +1
+            </button>
+            <button
+              onClick={() => this.handlePlayerAction("-1")}
+              disabled={isDisabledBtn}
+            >
+              -1
+            </button>
+            <button
+              onClick={() => this.handlePlayerAction("×2")}
+              disabled={isDisabledBtn}
+            >
+              ×2
+            </button>
+            <button
+              onClick={() => this.handlePlayerAction("÷2")}
+              disabled={isDisabledBtn}
+            >
+              ÷2
+            </button>
+          </div>
         </div>
       </div>
     );
